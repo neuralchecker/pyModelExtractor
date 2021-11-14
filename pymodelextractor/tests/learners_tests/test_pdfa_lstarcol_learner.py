0f1dccf99af69d361f4a052427cc245bfb193f89
DESKTOP-36QKRHL\franz <franzmayrojeda@gmail.com>
2021-11-14 19:47:14 -0300

    learning result now works for all learning algorithms

--- pymodelextractor/tests/learners_tests/test_pdfa_lstarcol_learner.py
+++ pymodelextractor/tests/learners_tests/test_pdfa_lstarcol_learner.py
@@ -14,41 +14,41 @@ class TestPDFALStarColLearner(unittest.TestCase):
     def test_tomitas_1(self):
         model = WeightedTomitasGrammars.get_automaton_1()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
 
     def test_tomitas_2(self):
         model = WeightedTomitasGrammars.get_automaton_2()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
 
     def test_tomitas_3(self):
         model = WeightedTomitasGrammars.get_automaton_3()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
 
     def test_tomitas_4(self):
         model = WeightedTomitasGrammars.get_automaton_4()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
 
     def test_tomitas_5(self):
         model = WeightedTomitasGrammars.get_automaton_5()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
 
     def test_tomitas_6(self):
         model = WeightedTomitasGrammars.get_automaton_6()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
 
     def test_tomitas_7(self):
         model = WeightedTomitasGrammars.get_automaton_7()
         teacher = PDFATeacher(model, 0)
-        extracted_model = self.learner.learn(teacher)
+        extracted_model = self.learner.learn(teacher).model
         self.assertEqual(model, extracted_model)
