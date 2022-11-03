5471407ef88d52d8565a43620e9f7ab3daddcf25
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2022-11-03 19:09:20 -0300

    Moore machines observation tables made

--- pymodelextractor/learners/observation_table_learners/mm_observation_table.py
+++ pymodelextractor/learners/observation_table_learners/mm_observation_table.py
@@ -18,34 +18,52 @@ class MMObservationTable:
         self.observations[sequence] = observationsRow
 
     def is_closed(self) -> bool:
-        raise NotImplementedError
+        red_values = self._get_red_values()
+        for sequence in self.blue:
+            blue_symbol = self.observations[sequence]
+            if red_values.get(blue_symbol) == None:
+                return False
+        return True
+
+    def _get_red_values(self) -> dict[list[Symbol], int]:
+        redValues: dict[list[Symbol], int]
+        redValues = {}
+        for sequence in self.red: 
+            redSymbol = self.observations[sequence]
+            if redValues.get(redSymbol) == None:
+                redValues[redSymbol] = 1
+        return redValues
 
     def find_inconsistency(self, alphabet: Alphabet) -> Union[TableInconsistency, None]:
-        raise NotImplementedError
+        redValues: dict(list[Symbol], Sequence)
+        redValues = {}
+        for row in self.red:
+            rowSymbols = self.observations[row]
+            redValue = redValues.get(rowSymbols)
+            if redValue != None:
+                inconsistency = self._are_inconsistent(redValue, row, alphabet)
+                if inconsistency != None:
+                    return inconsistency
+            else:
+                redValues[rowSymbols] = row
+        return None
 
+    def _are_inconsistent(self, sequence1: Sequence, sequence2: Sequence, alphabet: Alphabet)-> \
+            Union[TableInconsistency, None]:
+        for symbol in alphabet:
+            suffixedSequence1 = sequence1 + symbol
+            suffixedSequence2 = sequence2 + symbol
+            if self.observations[suffixedSequence1] != self.observations[suffixedSequence2]:
+                differenceSequence = None 
+                    # TODO FIND WHAT IS DIFFERENCE SEQUENCE
+                
+                return TableInconsistency(sequence1, sequence2, symbol, differenceSequence)
+        return None
+    
     def move_from_blue_to_red(self, sequence: Sequence):
         self.blue.remove(sequence)
         self.red.add(sequence)
 
-    def _inconsistency_between(self, sequence1: Sequence, sequence2: Sequence, alphabet: Alphabet) -> \
-            Union[TableInconsistency, None]:
-        for symbol in alphabet.symbols:
-            suffixedSequence1 = sequence1 + symbol
-            suffixedSequence2 = sequence2 + symbol
-            differenceSequence = self._observation_difference_between(
-                suffixedSequence1, suffixedSequence2)
-            if differenceSequence is not None:
-                return TableInconsistency(sequence1, sequence2, symbol, differenceSequence)
-        return None
-
-    def _observation_difference_between(self, sequence1: Sequence, sequence2: Sequence) -> Union[Sequence, None]:
-        observations1 = self.observations[sequence1]
-        observations2 = self.observations[sequence2]
-        assert len(observations1) == len(observations2)
-        for i in range(0, len(observations1)):
-            if observations1[i] != observations2[i]:
-                return self.exp[i]
-        return None
 
     def __str__(self):
         lines = ["\nObservation Table:",
