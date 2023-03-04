9c7a43b3bb7b3d2c16c4b2bbadccc79d73f7b5ca
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2023-03-04 20:24:30 -0300

    Refactor

--- pymodelextractor/teachers/generic_teacher.py
+++ pymodelextractor/teachers/generic_teacher.py
@@ -7,7 +7,7 @@ from pythautomata.base_types.alphabet import Alphabet
 
 class GenericTeacher(Teacher):
     def __init__(self, state_machine, comparison_strategy, w_cache = True):
-        self.state_machine = state_machine
+        self._state_machine = state_machine
         self._comparison_strategy = comparison_strategy
         self.log_hierachy = 'none'
         self._cache = {}
@@ -20,11 +20,11 @@ class GenericTeacher(Teacher):
 
     @property
     def alphabet(self) -> Alphabet:
-        return self.state_machine._alphabet
+        return self._state_machine._alphabet
 
     @property
     def output_alphabet(self) -> Alphabet:
-        return self.state_machine.output_alphabet
+        return self._state_machine.output_alphabet
 
     @property
     def equivalence_queries_count(self) -> int:
@@ -34,10 +34,10 @@ class GenericTeacher(Teacher):
         self._membership_queries_count += 1
 
         if not self._w_cache:
-            return self.state_machine.process_query(sequence)  
+            return self._state_machine.process_query(sequence)  
         
         if sequence not in self._cache:
-            result = self.state_machine.process_query(sequence)
+            result = self._state_machine.process_query(sequence)
             self._cache[sequence] = result
             return result
 
@@ -45,7 +45,7 @@ class GenericTeacher(Teacher):
 
     def equivalence_query(self, model) -> Tuple[bool, Union[Sequence, None]]:
         self._equivalence_queries_count += 1
-        counterexample = self._comparison_strategy.get_counterexample_between(model, self.state_machine)
+        counterexample = self._comparison_strategy.get_counterexample_between(model, self._state_machine)
         are_equivalent = counterexample is None
 
         return are_equivalent, counterexample
