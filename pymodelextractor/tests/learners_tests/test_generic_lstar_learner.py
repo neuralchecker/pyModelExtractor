4e6c971afeef411ca5946329e7b2f65a6ed6d170
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2023-03-04 21:31:16 -0300

    Added PAC comparison strategy for generic lstar

--- pymodelextractor/tests/learners_tests/test_generic_lstar_learner.py
+++ pymodelextractor/tests/learners_tests/test_generic_lstar_learner.py
@@ -1,9 +1,9 @@
-
+import time
 import unittest
 
 from pymodelextractor.teachers.generic_teacher import \
     GenericTeacher
-from pymodelextractor.learners.observation_table_learners.lstar_factory import LStarFactory
+from pymodelextractor.factories.lstar_factory import LStarFactory
 from pythautomata.automata_definitions.tomitas_grammars import TomitasGrammars
 from pythautomata.model_comparators.hopcroft_karp_comparison_strategy import \
     HopcroftKarpComparisonStrategy as ComparisonStrategy
@@ -11,74 +11,32 @@ from pythautomata.model_comparators.moore_machine_comparison_strategy import Moo
 from pythautomata.model_comparators.dfa_comparison_strategy import DFAComparisonStrategy
 from pythautomata.utilities.automata_converter import AutomataConverter
 from pythautomata.utilities.simple_dfa_generator import generate_dfa
+from pymodelextractor.teachers.pac_comparison_strategy import PACComparisonStrategy
 from pymodelextractor.utils.time_bound_utilities import is_unix_system
+from itertools import chain
 
 
 class TestGenericLStarLearner(unittest.TestCase):
     def test_generic_learner(self):
-        alphabet = TomitasGrammars.get_automaton_7()._alphabet
-        iters = 10
-        for i in range(2):
-            num = (i+1)*100
-            for _ in range(iters):
-                automaton = generate_dfa(alphabet, num)
-                moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
+        mergedAutomata = list(chain(TomitasGrammars.get_all_automata()))
+        for automaton in mergedAutomata:
+            moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
 
-                result = LStarFactory.get_dfa_lstar_learner().learn(GenericTeacher(automaton, DFAComparisonStrategy()))
-                assert ComparisonStrategy().are_equivalent(result.model, automaton)
+            result = LStarFactory.get_dfa_lstar_learner().learn(GenericTeacher(automaton, DFAComparisonStrategy()))
+            assert ComparisonStrategy().are_equivalent(result.model, automaton)
 
-                result = LStarFactory.get_moore_machine_lstar_learner().learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
-                assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
+            result = LStarFactory.get_moore_machine_lstar_learner().learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
+            assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
 
-    def test_bounded_generic_learner_tomitas1(self):
-        automaton = TomitasGrammars.get_automaton_1()
-        moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
+    def test_bouded_generic_learner_tomitas(self):
+        mergedAutomata = list(chain(TomitasGrammars.get_all_automata()))
+        for automaton in mergedAutomata:
+            result = LStarFactory.get_dfa_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
+            assert ComparisonStrategy().are_equivalent(result.model, automaton)
 
-        result = LStarFactory.get_dfa_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
-        assert ComparisonStrategy().are_equivalent(result.model, automaton)
-
-        result = LStarFactory.get_moore_machine_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
-        assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
-
-    def test_bounded_generic_learner_tomitas2(self):
-        automaton = TomitasGrammars.get_automaton_2()
-        moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
-
-        result = LStarFactory.get_dfa_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
-        assert ComparisonStrategy().are_equivalent(result.model, automaton)
-
-        result = LStarFactory.get_moore_machine_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
-        assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
-
-    def test_bounded_generic_learner_tomitas3(self):
-        automaton = TomitasGrammars.get_automaton_3()
-        moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
-
-        result = LStarFactory.get_dfa_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
-        assert ComparisonStrategy().are_equivalent(result.model, automaton)
-
-        result = LStarFactory.get_moore_machine_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
-        assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
-
-    def test_bounded_generic_learner_tomitas4(self):
-        automaton = TomitasGrammars.get_automaton_4()
-        moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
-
-        result = LStarFactory.get_dfa_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
-        assert ComparisonStrategy().are_equivalent(result.model, automaton)
-
-        result = LStarFactory.get_moore_machine_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
-        assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
-
-    def test_bounded_generic_learner_tomitas5(self):
-        automaton = TomitasGrammars.get_automaton_5()
-        moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
-
-        result = LStarFactory.get_dfa_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
-        assert ComparisonStrategy().are_equivalent(result.model, automaton)
-
-        result = LStarFactory.get_moore_machine_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
-        assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
+            moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
+            result = LStarFactory.get_moore_machine_lstar_learner(max_query_length=10, max_states=10).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
+            assert MooreMachineComparisonStrategy().are_equivalent(result.model, moore)
 
     def test_bounded_generic_learner_big_automaton(self):
         automaton = generate_dfa(TomitasGrammars.get_automaton_7()._alphabet, 100)
@@ -97,17 +55,24 @@ class TestGenericLStarLearner(unittest.TestCase):
         self.assertFalse(MooreMachineComparisonStrategy().are_equivalent(result.model, moore))
 
     def test_time_bounded_lstar(self):
-
         if is_unix_system():
             automaton = generate_dfa(TomitasGrammars.get_automaton_7()._alphabet, 10000)
             moore = AutomataConverter().convert_dfa_to_moore_machine(automaton)
 
-            
-            result = LStarFactory.get_dfa_lstar_learner(max_time=1).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
+            result = LStarFactory.get_dfa_lstar_learner(max_time=5).learn(GenericTeacher(automaton, DFAComparisonStrategy()))
             self.assertFalse(ComparisonStrategy().are_equivalent(result.model, automaton))
 
-            result = LStarFactory.get_moore_machine_lstar_learner(max_time=1).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
+            result = LStarFactory.get_moore_machine_lstar_learner(max_time=5).learn(GenericTeacher(moore, MooreMachineComparisonStrategy()))
             self.assertFalse(MooreMachineComparisonStrategy().are_equivalent(result.model, moore))
+
+    def test_pac_against_many_DFAs(self):
+        mergedAutomata = list(chain(TomitasGrammars.get_all_automata()))
+        for automaton in mergedAutomata:
+            teacher = GenericTeacher(automaton, PACComparisonStrategy(automaton, 0.005, 0.005, 20))
+            result = LStarFactory.get_dfa_lstar_learner().learn(teacher)
+            assert ComparisonStrategy().are_equivalent(result.model, automaton)
+        
+
 
 
         
