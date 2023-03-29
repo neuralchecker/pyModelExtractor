5ddeb03c2638900e3664d12ba549d341dc213bfd
Franz Mayr <franz.mayr.ojeda@gmail.com>
2023-03-28 22:10:47 -0300

    work in progress

---
+++ pymodelextractor/utils/csv_bool_data_loader.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from pymodelextractor.utils.data_loader import DataLoader
+from pythautomata.base_types.alphabet import Alphabet
+from pythautomata.base_types.sequence import Sequence
+from pythautomata.base_types.symbol import SymbolStr
+
+from pymodelextractor.utils.csv_data_loader import CsvDataLoader
+
+class CsvBoolDataLoader(CsvDataLoader):    
+    @abstractmethod
+    def value_to_output(self, value):  
+        return bool(value)
