340528a5f7e68591897108412caa48bf2e5f7142
DESKTOP-36QKRHL\franz <franzmayrojeda@gmail.com>
2023-04-13 13:58:03 -0300

    added cache code to the parent teacher class for simplicity

--- pymodelextractor/teachers/pac_batch_probabilistic_teacher.py
+++ pymodelextractor/teachers/pac_batch_probabilistic_teacher.py
@@ -10,41 +10,13 @@ from pymodelextractor.utils.data_loader import DataLoader
 from typing import Union
 import numpy as np
 from collections import OrderedDict
-from multiprocessing import Process, Manager
 class PACBatchProbabilisticTeacher(PACProbabilisticTeacher):
 
     def __init__(self, model: ProbabilisticModel, epsilon: float, delta: float,
                  comparator: FiniteAutomataComparator, sequence_generator: SequenceGenerator = None,
                  max_seq_length: float = 128, compute_epsilon_star: bool = True, parallel_cache = False, max_query_elements = 1_000_000, batch_size = 10_000, cache_from_dataloader:DataLoader = None):
-        super().__init__(model, comparator, epsilon, delta, sequence_generator, max_seq_length, compute_epsilon_star)
+        super().__init__(model, comparator, epsilon, delta, sequence_generator, max_seq_length, compute_epsilon_star, parallel_cache , max_query_elements, batch_size, cache_from_dataloader)
         assert (hasattr(model, 'get_last_token_weights_batch'))
-        self._parallel_cache = parallel_cache
-        self._max_query_elements = max_query_elements    
-        if self._parallel_cache:
-            manager = Manager()
-            self._cache = manager.dict()
-            self._job = Process(target=self.fill_cache, args=(self._cache, model,self._max_query_elements, batch_size)) 
-            self._job.start() 
-        if cache_from_dataloader is not None:
-            if not self._parallel_cache:
-                self._cache = dict()
-            self._cache.update(cache_from_dataloader.get_data())
-
-    def fill_cache(self, cache, model, max_query_elements, batch_size):
-        total_elements = 0
-        generator = self._sequence_generator.generate_all_words()
-        symbols = list(self.alphabet.symbols)
-        symbols.sort()
-        symbols = [self.terminal_symbol] + symbols
-
-        while total_elements<max_query_elements:
-            queries = []
-            for _ in range(batch_size):
-                queries.append(next(generator))                      
-            results = model.get_last_token_weights_batch(queries, symbols)                         
-            results_od = [OrderedDict(zip(symbols, x)) for x in results]
-            final_results  = dict(zip(queries, results_od))            
-            cache.update(final_results)
 
     def equivalence_query(self, aut: WeightedAutomaton) -> tuple[bool, Union[Sequence, None]]:
         self._equivalence_queries_count += 1
@@ -72,38 +44,7 @@ class PACBatchProbabilisticTeacher(PACProbabilisticTeacher):
         if errorCount > 0:
             self._calculate_epsilon_star_with(errorCount)
 
-        return counterexample is None, counterexample
+        return counterexample is None, counterexample    
 
-    def next_token_probabilities(self, sequence: Sequence) -> OrderedDict[Symbol, float]:
-        if self._parallel_cache:            
-            if sequence in self._cache:
-                return self._cache[sequence]
-        return super().next_token_probabilities(sequence)
 
-    def next_token_probabilities_batch(self, sequences):
-        symbols = list(self.alphabet.symbols)
-        symbols.sort()
-        symbols = [self.terminal_symbol] + symbols
-
-        if self._parallel_cache:
-            queries = set()
-            results_already_in_cache = dict()
-            for sequence in sequences:
-                if sequence not in self._cache:
-                    queries.add(sequence)
-                else:
-                    results_already_in_cache[sequence] = self._cache[sequence]
-            results = self._target_model.get_last_token_weights_batch(queries, symbols)   
-            results_od = [OrderedDict(zip(symbols, x)) for x in results]
-            final_results  = dict(zip(queries, results_od))
-            self._cache.update(final_results)
-            final_results.update(results_already_in_cache)
-        else:        
-            results = self._target_model.get_last_token_weights_batch(sequences, symbols)
-            results_od = [OrderedDict(zip(symbols, x)) for x in results]
-            final_results = zip(sequences, results_od)
-        return final_results
     
-    def __del__(self):
-        if self._parallel_cache:
-            self._job.terminate()
--- pymodelextractor/teachers/probabilistic_teacher.py
+++ pymodelextractor/teachers/probabilistic_teacher.py
@@ -5,32 +5,51 @@ from pythautomata.base_types.symbol import Symbol
 from pythautomata.base_types.alphabet import Alphabet
 from pythautomata.base_types.sequence import Sequence
 from pythautomata.automata.wheighted_automaton_definition.weighted_automaton import WeightedAutomaton
+from pymodelextractor.utils.data_loader import DataLoader
+from pythautomata.abstract.probabilistic_model import ProbabilisticModel
 
+
+from collections import OrderedDict
+from multiprocessing import Process, Manager
 from typing import Union
 
 
 class ProbabilisticTeacher(ABC):
 
-    def __init__(self):
+    def __init__(self, model: ProbabilisticModel, parallel_cache = False, max_query_elements = 1_000_000, batch_size = 10_000, cache_from_dataloader:DataLoader = None):
         self._equivalence_queries_count = 0
-        self._last_token_weight_queries_count = 0
+        self._last_token_weight_queries_count = 0        
+        self._target_model = model
+        self._parallel_cache = parallel_cache
+        self._max_query_elements = max_query_elements   
+        if self._parallel_cache:
+            manager = Manager()
+            self._cache = manager.dict()
+            self._job = Process(target=self.fill_cache, args=(self._cache, model,self._max_query_elements, batch_size)) 
+            self._job.start() 
+        if cache_from_dataloader is not None:
+            if not self._parallel_cache:
+                self._cache = dict()
+            self._cache.update(cache_from_dataloader.get_data())
 
-    @abstractmethod
-    def sequence_weight(self, sequence: Sequence) -> float:
-        raise NotImplementedError
+    def sequence_weight(self, sequence: Sequence):
+        return self._target_model.sequence_weight(sequence)
 
     @abstractmethod
     def log_sequence_weight(self, sequence: Sequence) -> float:
         raise NotImplementedError
 
-    @abstractmethod
-    def last_token_weights(self, sequence: Sequence, required_suffixes: list[Sequence]) -> list[float]:
-        raise NotImplementedError
+    def last_token_weights(self, sequence: Sequence, required_suffixes: list[Sequence]):
+        self._last_token_weight_queries_count +=len(required_suffixes)            
+        return self._target_model.get_last_token_weights(sequence, required_suffixes)
 
     def equivalence_query(self, aut: WeightedAutomaton) -> tuple[bool, Union[Sequence, None]]:
         raise NotImplementedError
 
     def next_token_probabilities(self, sequence: Sequence) -> OrderedDict[Symbol, float]:
+        if self._parallel_cache:            
+            if sequence in self._cache:
+                return self._cache[sequence]
         symbols = list(self.alphabet.symbols)
         symbols.sort()
         symbols = [self.terminal_symbol] + symbols
@@ -62,3 +81,52 @@ class ProbabilisticTeacher(ABC):
     @property
     def last_token_weight_queries_count(self):
         return self._last_token_weight_queries_count
+
+    def next_token_probabilities_batch(self, sequences):
+        assert hasattr(self._target_model, "get_last_token_weights_batch")
+        symbols = list(self.alphabet.symbols)
+        symbols.sort()
+        symbols = [self.terminal_symbol] + symbols
+
+        if self._parallel_cache:
+            queries = set()
+            results_already_in_cache = dict()
+            for sequence in sequences:
+                if sequence not in self._cache:
+                    queries.add(sequence)
+                else:
+                    results_already_in_cache[sequence] = self._cache[sequence]
+            results = self._target_model.get_last_token_weights_batch(queries, symbols)   
+            results_od = [OrderedDict(zip(symbols, x)) for x in results]
+            final_results  = dict(zip(queries, results_od))
+            self._cache.update(final_results)
+            final_results.update(results_already_in_cache)
+        else:        
+            results = self._target_model.get_last_token_weights_batch(sequences, symbols)
+            results_od = [OrderedDict(zip(symbols, x)) for x in results]
+            final_results = zip(sequences, results_od)
+        return final_results
+    
+    def fill_cache(self, cache, model, max_query_elements, batch_size):
+        total_elements = 0
+        generator = self._sequence_generator.generate_all_words()
+        symbols = list(self.alphabet.symbols)
+        symbols.sort()
+        symbols = [self.terminal_symbol] + symbols
+        use_batch = hasattr(self._target_model, "get_last_token_weights_batch")
+        while total_elements<max_query_elements:
+            queries = []
+            if use_batch: 
+                for _ in range(batch_size):
+                    queries.append(next(generator))                                 
+                results = model.get_last_token_weights_batch(queries, symbols)     
+            else:
+                queries = [next(generator)]
+                results = [self.last_token_weights(queries[0], symbols)]                
+            results_od = [OrderedDict(zip(symbols, x)) for x in results]
+            final_results  = dict(zip(queries, results_od))            
+            cache.update(final_results)
+
+    def __del__(self):
+        if hasattr(self, "_parallel_cache") and self._parallel_cache:
+            self._job.terminate()
